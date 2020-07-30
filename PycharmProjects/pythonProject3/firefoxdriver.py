894174212181804251d8f8252a1b07f9142293d1
Oluniyi <niyi@kairoscs.com>
2020-07-30 10:37:52 -0400

    Python Selenium Code
    Code for Python developmen

---
+++ PycharmProjects/pythonProject3/firefoxdriver.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+
+class RuffTests():
+    #def test1():
+        #driver =webdriver.Chrome()
+        #driver.get("https://www.cnn.com")
+        #driver.fullscreen_window()
+        #driver.quit
+
+    def test2():
+        driver =webdriver.Firefox()
+        driver.get("https://www.kairoscs.com")
+        driver.fullscreen_window()
+        driver.refresh()
+        driver.quit()
+
+
+
+ff = RuffTests
+#ff.test1()
+ff.test2()
+
+
