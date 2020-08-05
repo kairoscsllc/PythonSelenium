5425bba1a6d99ecba83a0affd0e5b58f5bceb443
Oluniyi <niyi@kairoscs.com>
2020-08-04 23:44:44 -0400

    Python New
    Python New

--- PycharmProjects/pythonProject3/Chromexpathdriver.py
+++ PycharmProjects/pythonProject3/Chromexpathdriver.py
@@ -3,12 +3,13 @@ from selenium.webdriver.common.by import By
 import time
 
 
-class TestScenarios():
-    def testcase1():
+class TestScenarios:
+    def testcase1(self):
         driver = webdriver.Chrome()
         driver.get("https://www.cnn.com")
         driver.fullscreen_window()
         driver.find_element(By.XPATH, "/html/body/div[5]/div/div/header/div/div[1]/div/div[2]/nav/ul/li[9]/a").click()
+        driver.get_screenshot_as_png("screensh")
         time.sleep(20)
         driver.quit
 
@@ -16,18 +17,18 @@ class TestScenarios():
         driver = webdriver.Chrome()
         driver.get("https://www.scaledagileframework.com/system-team/")
         driver.find_element(By.XPATH, "/html/body/div[3]/div[1]/div[1]/div[2]/div/nav/div/div[1]/ul/li[6]/a").click()
+        print (driver.get_screenshot_as_file("screenshottogo4.png"))
         time.sleep(20)
         driver.quit
 
 
-    def testcase3() -> object:
+    def testcase3():
         driver = webdriver.Chrome()
         driver.get("https://www.espn.com")
-        driver.find_element(By.XPATH, "//span[@class='link-text'][contains(text(),'Watch')]").click
+        driver.find_element(By.XPATH, "//span[@class='link-text'][contains(text(),'Watch')").click
+        driver.get_screenshot_as_file("screenshottogo3.png"
 
-
-
-Run = TestScenarios
-# TestScenarios.testcase1()
-# TestScenarios.testcase2()
-TestScenarios.testcase3()
+ff = TestScenarios
+ff.testcase1()
+ff.testcase2()
+ff.testcase3()
---
+++ PycharmProjects/pythonProject5/main.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class Testing123:
+    def testcase():
+        driver = webdriver.Chrome()
+        driver.get("https://www.cnn.com")
+        driver.get_screenshot_as_file("CNNscreen6.png")
+        driver.find_element(By.XPATH, "/html/body/div[5]/div/div/header/div/div[1]/div/div[2]/nav/ul/li[9]/a").click()
+        time.sleep(15)
+        driver.get_screenshot_as_file("CNNscreenChrome.png")
+        driver.close()
+
+    def testcase2():
+        driver = webdriver.Firefox()
+        driver.get("https://www.nfl.com")
+        driver.find_element(By.XPATH, "/html/body/div[4]/header/div/nav[1]/ul/li[1]/a/span").click()
+        time.sleep(10)
+        driver.get_screenshot_as_file("NFLscreenFirefox.png")
+        driver.quit()
+
+
+    def testcase3():
+        driver = webdriver.Safari()
+        driver.get("https://www.fox.com")
+        driver.fullscreen_window()
+        time.sleep(10)
+        driver.get_screenshot_as_file("FOXScreen.png")
+        driver.quit()
+
+
+Testing123.testcase()
+Testing123.testcase2()
+Testing123.testcase3()
+
